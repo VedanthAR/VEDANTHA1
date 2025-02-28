9cf4ea9b6f998890498293e82fbac4dcd02c5db1
VedanthAR <vedanth.r@s.amity>
2025-02-28 12:06:32 +0530

    enter the commit message for your changes. Lines starting

--- python.py
+++ python.py
@@ -1,9 +1,20 @@
-A=int(input("enter the number A")
-B=int(input("enter the number B")
-Add=A+B
-sub=A-B
-mul=A*B
-print(sub)
-print(Add)
-print(mul)
+print('note A should be greater than B')
+A=int(input('enter the value of A'))
+B=int(input('enter the value of B'))
+op=int(input('enter the operation 1=+,2=*,3=/,4=-'))
+if(op==1):
+ C=A+B
+ print(C)
+elif(op==2):
+ C=A*B
+ print(C)
+elif(op==3):
+ C=A/B
+ print(C)
+elif(op==4):
+ C=A-B
+ print(C)
+elif(op==0):
+ print('invalid input')
+
 
