7c6878a0307741c0b4e3c7d81af331618d6b6409
VedanthAR <vedanth.r@s.amity>
2025-03-07 09:55:07 +0530

    hello

--- Vedantha.py
+++ Vedantha.py
@@ -3,3 +3,9 @@ a=int(input('enter the value of A'))
 b=int(input('enter the value of B'))
 c=a+b
 print(c)
+print("naveen")
+d=int(input('enter the value of D'))
+e=int(input('enter the value of E'))
+f=d*e
+print(f)
+
