02762af9bdcd115d1bbb6982cec7189391ba2048
VedanthAR <vedanth.r@s.amity>
2025-05-15 22:21:59 +0530

    vedanth git

--- Vedantha.py
+++ Vedantha.py
@@ -8,4 +8,21 @@ d=int(input('enter the value of D'))
 e=int(input('enter the value of E'))
 f=d*e
 print(f)
-
+s="hello world"
+sub="world"
+print(sub in s)
+s="hi"
+s=s.lower()
+vowels="aeiou"
+v1=s[0] in vowels
+v2=s[1] in vowels
+vowels_count=2-vowels_count
+print("vowels:",vowels_count)
+print("consonants:",consonants_count)
+s1="listen"
+s2="silent"
+s1=".join(sorted(s1.replace("","").lower()))
+s2=".join(sorted(s2.replace("","").lower()))
+print(s1==s2)
+s="aabbcc"
+result=s[0]
--- Vedantha1.py
+++ Vedantha1.py
@@ -4,4 +4,6 @@ n=int(input('enter the value of n:'))
 for i in range(1,n+1):
     fact=fact*i
     print(fact)
+print("hi hello world")
+print("this is a simple illustration of commiting on gitbash")
 
